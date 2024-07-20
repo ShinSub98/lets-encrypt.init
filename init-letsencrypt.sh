cc61afbff5442c8601ca75feaea2b4aa6ea28d6c
Jeong Sub Shin <111645616+ShinSub98@users.noreply.github.com>
2024-07-21 00:54:09 +0900

    Update init-letsencrypt.sh
    <docs>:add script deleting data folder

--- init-letsencrypt.sh
+++ init-letsencrypt.sh
@@ -11,6 +11,8 @@ data_path="./data/certbot"
 email="EMAIL" # Adding a valid address is strongly recommended
 staging=1 # Set to 1 if you're testing your setup to avoid hitting request limits
 
+sudo rm -rf data
+
 if [ -d "$data_path" ]; then
   read -p "Existing data found for $domains. Continue and replace existing certificate? (y/N) " decision
   if [ "$decision" != "Y" ] && [ "$decision" != "y" ]; then
